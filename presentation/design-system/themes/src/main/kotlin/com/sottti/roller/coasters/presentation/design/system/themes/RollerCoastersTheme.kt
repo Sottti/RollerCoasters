2bb3acb8610a68170b97e4953f454feb2f826b70
Pablo Costa <sottti@gmail.com>
2025-01-22 18:43:26 +0100

    Fixes status bar icon colors.

--- presentation/design-system/themes/src/main/kotlin/com/sottti/roller/coasters/presentation/design/system/themes/RollerCoastersTheme.kt
+++ presentation/design-system/themes/src/main/kotlin/com/sottti/roller/coasters/presentation/design/system/themes/RollerCoastersTheme.kt
@@ -1,11 +1,14 @@
 package com.sottti.roller.coasters.presentation.design.system.themes
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.view.WindowInsetsControllerCompat
 import com.sottti.roller.coasters.data.settings.di.provideSettingsRepository
 import com.sottti.roller.coasters.domain.settings.model.Theme
 import com.sottti.roller.coasters.presentation.design.system.dimensions.resolution.DimensionsLocalProvider
@@ -29,6 +32,8 @@ public fun RollerCoastersTheme(
         .map { theme -> theme.toDarkTheme(isSystemInDarkTheme) }
         .collectAsState(initial = isSystemInDarkTheme)
 
+    UpdateStatusBarColors(darkTheme = darkTheme)
+
     DimensionsLocalProvider {
         BaseTheme(
             dynamicColor = dynamicColor,
@@ -44,4 +49,18 @@ private fun Theme.toDarkTheme(
     Theme.DarkTheme -> true
     Theme.LightTheme -> false
     Theme.SystemTheme -> isSystemInDarkTheme
+}
+
+@Composable
+private fun UpdateStatusBarColors(
+    darkTheme: Boolean,
+) {
+    val context = LocalContext.current
+    val activity = context as? Activity
+    LaunchedEffect(darkTheme) {
+        activity?.window?.let { window ->
+            val insetsController = WindowInsetsControllerCompat(window, window.decorView)
+            insetsController.isAppearanceLightStatusBars = !darkTheme
+        }
+    }
 }
