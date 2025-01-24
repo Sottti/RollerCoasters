c4ddc306b8325ebd40c029af343753e8884e1c4a
Pablo Costa <sottti@gmail.com>
2025-01-24 13:07:11 +0100

    Annotates IconState as Immutable

--- presentation/design-system/icons/src/main/kotlin/co/cuvva/presentation/design/system/icons/model/IconState.kt
+++ presentation/design-system/icons/src/main/kotlin/co/cuvva/presentation/design/system/icons/model/IconState.kt
@@ -2,7 +2,9 @@ package co.cuvva.presentation.design.system.icons.model
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.runtime.Immutable
 
+@Immutable
 public data class IconState(
     @DrawableRes val resId: Int,
     @StringRes val descriptionResId: Int,
