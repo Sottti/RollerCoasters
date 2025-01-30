6aad16ab6ebe6f538a9d3053a1d8652d6ef95aeb
Pablo Costa <sottti@gmail.com>
2025-01-30 23:01:04 +0100

    Includes plugin for detecting dep updated versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.dagger.hilt) apply false
+    alias(libs.plugins.dependency.versions) apply true
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.kotlin.kapt) apply false
