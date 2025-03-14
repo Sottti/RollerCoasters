e7a32eee61bdc6d6dd4b6d498da043fd33719715
Pablo Costa Tirado <sottti@gmail.com>
2025-03-14 12:55:06 +0100

    Removes some warnings.

--- presentation/design-system/chip/build.gradle.kts
+++ presentation/design-system/chip/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 android {
     namespace = "co.sottti.roller.coasters.presentation.design.system.chip"
     buildFeatures { compose = true }
+    @Suppress("UnstableApiUsage")
     composeOptions { kotlinCompilerExtensionVersion = kotlinCompilerExtensionVersion }
 }
 
--- presentation/design-system/dialogs/build.gradle.kts
+++ presentation/design-system/dialogs/build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
 android {
     namespace = "co.sottti.roller.coasters.presentation.design.system.dialogs"
     buildFeatures { compose = true }
+    @Suppress("UnstableApiUsage")
     composeOptions { kotlinCompilerExtensionVersion = kotlinCompilerExtensionVersion }
 }
 
