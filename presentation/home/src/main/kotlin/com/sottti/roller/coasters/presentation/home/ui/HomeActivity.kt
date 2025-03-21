0e6264bf328adec51238dc95ff2343f999a2727c
Pablo Costa Tirado <sottti@gmail.com>
2025-03-21 18:44:01 +0100

    Removes unused imports.

--- presentation/home/src/main/kotlin/com/sottti/roller/coasters/presentation/home/ui/HomeActivity.kt
+++ presentation/home/src/main/kotlin/com/sottti/roller/coasters/presentation/home/ui/HomeActivity.kt
@@ -6,15 +6,8 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.ProvidableCompositionLocal
-import androidx.compose.runtime.staticCompositionLocalOf
-import com.sottti.roller.coasters.domain.settings.model.colorContrast.ResolvedColorContrast
-import com.sottti.roller.coasters.domain.settings.model.dynamicColor.ResolvedDynamicColor
-import com.sottti.roller.coasters.domain.settings.usecase.colorContrast.ObserveResolvedColorContrast
-import com.sottti.roller.coasters.domain.settings.usecase.dynamicColor.ObserveResolvedDynamicColor
 import com.sottti.roller.coasters.presentation.design.system.themes.RollerCoastersTheme
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.flowOf
 
 @AndroidEntryPoint
 internal class HomeActivity : AppCompatActivity() {
