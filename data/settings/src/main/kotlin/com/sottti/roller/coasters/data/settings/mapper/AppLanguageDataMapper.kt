332acd7a13934528dbcac995888e316febb4cc55
Pablo Costa Tirado <sottti@gmail.com>
2025-04-02 22:52:58 +0200

    Updates a few deps. Removes unused funs

--- data/settings/src/main/kotlin/com/sottti/roller/coasters/data/settings/mapper/AppLanguageDataMapper.kt
+++ data/settings/src/main/kotlin/com/sottti/roller/coasters/data/settings/mapper/AppLanguageDataMapper.kt
@@ -16,35 +16,6 @@ internal const val LOCALE_SPAIN_TAG = "es-ES"
 @VisibleForTesting
 internal const val LOCALE_UK_TAG = "en-GB"
 
-@VisibleForTesting
-internal const val ENGLISH_GB_KEY = "english_gb_language"
-
-@VisibleForTesting
-internal const val GALICIAN_KEY = "galician_language"
-
-@VisibleForTesting
-internal const val SPANISH_SPAIN_KEY = "spanish_spain_language"
-
-@VisibleForTesting
-internal const val SYSTEM_KEY = "system_language"
-
-internal val AppLanguage.key: String
-    get() = when (this) {
-        AppLanguage.EnglishGb -> ENGLISH_GB_KEY
-        AppLanguage.Galician -> GALICIAN_KEY
-        AppLanguage.SpanishSpain -> SPANISH_SPAIN_KEY
-        AppLanguage.System -> SYSTEM_KEY
-    }
-
-internal fun String.toAppLanguage(): AppLanguage =
-    when (this) {
-        ENGLISH_GB_KEY -> AppLanguage.EnglishGb
-        GALICIAN_KEY -> AppLanguage.Galician
-        SPANISH_SPAIN_KEY -> AppLanguage.SpanishSpain
-        SYSTEM_KEY -> AppLanguage.System
-        else -> AppLanguage.System
-    }
-
 internal fun AppLanguage.toLocaleList(): LocaleListCompat = when (this) {
     AppLanguage.EnglishGb -> forLanguageTags(LOCALE_UK_TAG)
     AppLanguage.Galician -> forLanguageTags("$LOCALE_GALICIA_TAG,$LOCALE_SPAIN_TAG")
