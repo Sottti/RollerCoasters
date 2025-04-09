b82f1c559c3236a86792fef7781c7c00e0bea67c
Pablo Costa Tirado <sottti@gmail.com>
2025-04-09 13:23:40 +0200

    Updates the InformativeUi.kt previews.

--- presentation/informative/src/main/kotlin/com/sottti/roller/coasters/presentation/informative/InformativeUi.kt
+++ presentation/informative/src/main/kotlin/com/sottti/roller/coasters/presentation/informative/InformativeUi.kt
@@ -2,7 +2,6 @@ package com.sottti.roller.coasters.presentation.informative
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,7 +15,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import co.cuvva.roller.coasters.presentation.design.system.text.Text
-import com.roller.coasters.presentation.design.system.illustrations.data.Illustrations.BrokenTrack
 import com.roller.coasters.presentation.design.system.illustrations.model.IllustrationState
 import com.roller.coasters.presentation.design.system.illustrations.ui.Illustration
 import com.sottti.roller.coasters.presentation.design.system.colors.color.colors
@@ -34,54 +32,34 @@ public fun InformativeUi(
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(dimensions.padding.medium)
+            .padding(dimensions.padding.medium),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Illustration(
-            state = BrokenTrack.state,
+            state = illustration,
             modifier = Modifier
                 .weight(0.5f)
                 .fillMaxWidth()
         )
-        Column(
-            modifier = modifier
-                .fillMaxWidth()
-                .weight(0.25f),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center,
+
+        Text.Display.Small(
+            text = stringResource(primaryText),
+            textColor = colors.onBackground,
+        )
+        Spacer(modifier = Modifier.height(dimensions.padding.medium))
+        Text.Body.Medium(
+            text = stringResource(secondaryText),
+            textColor = colors.onBackground,
+            textAlign = TextAlign.Center,
+        )
+
+        Spacer(modifier = Modifier.weight(0.25f))
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = onButtonClick
         ) {
-            PrimaryText(primaryText)
-            Spacer(modifier = Modifier.height(dimensions.padding.medium))
-            SecondaryText(secondaryText)
-        }
-        Box(
-            contentAlignment = Alignment.BottomCenter,
-            modifier = modifier
-                .padding(dimensions.padding.medium)
-                .weight(0.25f),
-        ) {
-            Button(
-                modifier = Modifier.fillMaxWidth(),
-                onClick = {},
-            ) {
-                Text.Vanilla(buttonText)
-            }
+            Text.Vanilla(buttonText)
         }
     }
-}
-
-@Composable
-private fun PrimaryText(primaryText: Int) {
-    Text.Display.Small(
-        text = stringResource(primaryText),
-        textColor = colors.onBackground,
-    )
-}
-
-@Composable
-private fun SecondaryText(secondaryText: Int) {
-    Text.Body.Medium(
-        text = stringResource(secondaryText),
-        textColor = colors.onBackground,
-        textAlign = TextAlign.Center,
-    )
 }
