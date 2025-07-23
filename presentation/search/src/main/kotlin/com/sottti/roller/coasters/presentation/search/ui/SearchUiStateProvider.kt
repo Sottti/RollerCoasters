cd65aa0b95f9105c6174797a1e4a9e188467b231
Pablo Costa Tirado <sottti@gmail.com>
2025-07-23 12:02:21 +0200

    Tweaks on SearchUiStateProvider.kt

--- presentation/search/src/main/kotlin/com/sottti/roller/coasters/presentation/search/ui/SearchUiStateProvider.kt
+++ presentation/search/src/main/kotlin/com/sottti/roller/coasters/presentation/search/ui/SearchUiStateProvider.kt
@@ -8,64 +8,56 @@ import com.sottti.roller.coasters.domain.fixtures.rollerCoaster
 import com.sottti.roller.coasters.presentation.search.R
 import com.sottti.roller.coasters.presentation.search.model.SearchBarViewState
 import com.sottti.roller.coasters.presentation.search.model.SearchPreviewState
+import com.sottti.roller.coasters.presentation.search.model.SearchResultViewState
 import com.sottti.roller.coasters.presentation.search.model.SearchViewState
 import com.sottti.roller.coasters.presentation.search.model.toViewState
 
 internal class SearchUiStateProvider : PreviewParameterProvider<SearchPreviewState> {
     override val values: Sequence<SearchPreviewState> = sequenceOf(
-        idleState,
-        resultsState,
+        initialState,
+        loadedState,
         loadingState,
     )
 }
 
-private val idleState = SearchPreviewState(
-    onAction = {},
-    onNavigateToRollerCoaster = {},
-    onNavigateToSettings = {},
-    paddingValues = PaddingValues(),
-    state = SearchViewState(
-        searchBar = SearchBarViewState(
-            hint = R.string.search_hint,
-            query = null,
-            showClearIcon = false,
-        ),
-        loading = false,
-        results = emptyList(),
+private val initialState = searchPreviewState(
+    searchBar = searchBarState(query = null, showClearIcon = false)
+)
+
+private val loadingState = searchPreviewState(
+    searchBar = searchBarState(query = COASTER_NAME.take(3), showClearIcon = true),
+    loading = true
+)
+
+private val loadedState = searchPreviewState(
+    searchBar = searchBarState(query = COASTER_NAME, showClearIcon = true),
+    results = listOf(
+        rollerCoaster().toViewState(),
+        anotherRollerCoaster().toViewState(),
     ),
 )
 
-private val resultsState = SearchPreviewState(
+private fun searchPreviewState(
+    loading: Boolean = false,
+    results: List<SearchResultViewState> = emptyList(),
+    searchBar: SearchBarViewState,
+) = SearchPreviewState(
     onAction = {},
     onNavigateToRollerCoaster = {},
     onNavigateToSettings = {},
     paddingValues = PaddingValues(),
     state = SearchViewState(
-        searchBar = SearchBarViewState(
-            hint = R.string.search_hint,
-            query = COASTER_NAME,
-            showClearIcon = true,
-        ),
-        loading = false,
-        results = listOf(
-            rollerCoaster().toViewState(),
-            anotherRollerCoaster().toViewState(),
-        ),
+        searchBar = searchBar,
+        loading = loading,
+        results = results,
     ),
 )
 
-private val loadingState = SearchPreviewState(
-    onAction = {},
-    onNavigateToRollerCoaster = {},
-    onNavigateToSettings = {},
-    paddingValues = PaddingValues(),
-    state = SearchViewState(
-        searchBar = SearchBarViewState(
-            hint = R.string.search_hint,
-            query = "Dr",
-            showClearIcon = true,
-        ),
-        loading = true,
-        results = emptyList(),
-    ),
+private fun searchBarState(
+    query: String?,
+    showClearIcon: Boolean,
+) = SearchBarViewState(
+    hint = R.string.search_hint,
+    query = query,
+    showClearIcon = showClearIcon,
 )
