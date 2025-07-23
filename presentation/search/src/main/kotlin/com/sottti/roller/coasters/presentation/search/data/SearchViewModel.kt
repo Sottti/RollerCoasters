390289969d3ad9ac01255ebb493eca5d2439a1f9
Pablo Costa Tirado <sottti@gmail.com>
2025-07-23 11:17:46 +0200

    Tweaks on SearchViewModel.kt

--- presentation/search/src/main/kotlin/com/sottti/roller/coasters/presentation/search/data/SearchViewModel.kt
+++ presentation/search/src/main/kotlin/com/sottti/roller/coasters/presentation/search/data/SearchViewModel.kt
@@ -3,6 +3,7 @@ package com.sottti.roller.coasters.presentation.search.data
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.github.michaelbull.result.getOrElse
+import com.sottti.roller.coasters.domain.roller.coasters.model.RollerCoaster
 import com.sottti.roller.coasters.domain.roller.coasters.model.SearchQuery
 import com.sottti.roller.coasters.domain.roller.coasters.usecase.SearchRollerCoasters
 import com.sottti.roller.coasters.presentation.search.R
@@ -18,7 +19,12 @@ import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -27,14 +33,13 @@ import javax.inject.Inject
 internal class SearchViewModel @Inject constructor(
     private val searchRollerCoasters: SearchRollerCoasters,
 ) : ViewModel() {
-    private val initialViewState: SearchViewState =
-        SearchViewState(
-            SearchBarViewState(
-                hint = R.string.search_hint,
-                query = null,
-                showClearIcon = false,
-            )
+    private val initialViewState: SearchViewState = SearchViewState(
+        SearchBarViewState(
+            hint = R.string.search_hint,
+            query = null,
+            showClearIcon = false,
         )
+    )
     private val _state = MutableStateFlow(initialViewState)
     val state: StateFlow<SearchViewState> = _state.asStateFlow()
 
@@ -49,22 +54,25 @@ internal class SearchViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            state
-                .map { it.searchBar.query.orEmpty().trim() }
+            state.map { viewState -> viewState.searchBar.query.orEmpty().trim() }
                 .distinctUntilChanged()
                 .debounce(300)
                 .flatMapLatest { query ->
-                    if (query.isBlank()) {
-                        flowOf(emptyList())
-                    } else {
-                        _state.loading()
-                        flow {
-                            emit(searchRollerCoasters(SearchQuery(query)).getOrElse { emptyList() })
+                    when {
+                        query.isBlank() -> flowOf(emptyList())
+                        else -> {
+                            _state.loading()
+                            flow {
+                                val searchResults =
+                                    searchRollerCoasters(SearchQuery(query)).getOrElse { emptyList() }
+                                emit(searchResults)
+                            }
                         }
                     }
-                }
-                .collect { results ->
-                    _state.notLoading().updateResults(results.map { it.toViewState() })
+                }.collect { searchResults ->
+                    _state
+                        .notLoading()
+                        .updateResults(searchResults.map(RollerCoaster::toViewState))
                 }
         }
     }
