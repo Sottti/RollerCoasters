9dc230468ec67877013b40824fb6298396f40ee3
Pablo Costa <sottti@gmail.com>
2025-07-25 14:00:43 +0200

    Show light theme previews before dark

--- presentation/previews/src/main/kotlin/com/sottti/roller/coasters/presentation/previews/PreviewConstants.kt
+++ presentation/previews/src/main/kotlin/com/sottti/roller/coasters/presentation/previews/PreviewConstants.kt
@@ -2,6 +2,6 @@ package com.sottti.roller.coasters.presentation.previews
 
 import androidx.compose.ui.tooling.preview.Devices
 
-internal const val darkThemeGroup = "Dark Theme"
+internal const val lightThemeGroup = "A - Light Theme"
+internal const val darkThemeGroup = "B - Dark Theme"
 internal const val device = Devices.PIXEL_9_PRO_XL
-internal const val lightThemeGroup = "Light Theme"
