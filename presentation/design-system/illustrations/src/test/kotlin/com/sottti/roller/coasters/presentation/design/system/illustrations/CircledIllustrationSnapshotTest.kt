16e072db6a542201536a3a6501b2859fb5b2cb2f
Pablo Costa Tirado <sottti@gmail.com>
2025-07-30 17:34:04 +0200

    Adds CircledIllustrationSnapshotTest.kt

---
+++ presentation/design-system/illustrations/src/test/kotlin/com/sottti/roller/coasters/presentation/design/system/illustrations/CircledIllustrationSnapshotTest.kt
@@ -0,0 +1,42 @@
+package com.sottti.roller.coasters.presentation.design.system.illustrations
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import com.android.ide.common.rendering.api.SessionParams
+import com.android.resources.NightMode
+import com.sottti.roller.coasters.presentation.design.system.illustrations.ui.CircledIllustrationPreview
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+internal class CircledIllustrationSnapshotTest(
+    nightMode: NightMode,
+) {
+
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.Companion.PIXEL_6_PRO.copy(nightMode = nightMode),
+        renderingMode = SessionParams.RenderingMode.SHRINK,
+        showSystemUi = false,
+        theme = "Theme.RollerCoasters",
+    )
+
+    @Test
+    fun snapshotTest() {
+        paparazzi.snapshot {
+            CircledIllustrationPreview()
+        }
+    }
+
+    companion object Companion {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> =
+            listOf(
+                arrayOf(NightMode.NOTNIGHT),
+                arrayOf(NightMode.NIGHT),
+            )
+    }
+}
