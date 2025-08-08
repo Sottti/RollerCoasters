45df5d966e86b6334760279eb750179e99d6c47f
Pablo Costa Tirado <sottti@gmail.com>
2025-08-09 01:29:21 +0200

    Removes the TodoList.kt file.

--- TodoList.kt
+++
@@ -1,7 +0,0 @@
-object TodoList {
-    // TODO: Navigation Bar on lower API levels
-    // TODO: State vs State vs nothing on State models
-    // TODO: State on same file vs separate files
-    // TODO: Previews in a different file or in the same file??
-    // TODO: StateProvider vs StateProvider vs PreviewProvider
-}
