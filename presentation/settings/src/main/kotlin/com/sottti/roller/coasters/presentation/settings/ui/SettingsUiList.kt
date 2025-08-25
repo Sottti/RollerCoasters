1b2087f2b1008582d9acd28bbb1972c5a7b590ed
Pablo Costa Tirado <sottti@gmail.com>
2025-08-25 15:04:19 +0200

    Revamps settings UI.

--- presentation/settings/src/main/kotlin/com/sottti/roller/coasters/presentation/settings/ui/SettingsUiList.kt
+++ presentation/settings/src/main/kotlin/com/sottti/roller/coasters/presentation/settings/ui/SettingsUiList.kt
@@ -1,18 +1,24 @@
 package com.sottti.roller.coasters.presentation.settings.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.material3.Card
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.ListItemDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.NestedScrollConnection
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import com.sottti.roller.coasters.presentation.design.system.dimensions.dimensions
 import com.sottti.roller.coasters.presentation.design.system.icons.model.IconState
+import com.sottti.roller.coasters.presentation.design.system.icons.ui.circledIcon.CircledIcon
 import com.sottti.roller.coasters.presentation.design.system.icons.ui.icon.Icon
 import com.sottti.roller.coasters.presentation.design.system.progress.indicators.ProgressIndicator
 import com.sottti.roller.coasters.presentation.design.system.progress.indicators.ProgressIndicatorSize
@@ -31,6 +37,7 @@ import com.sottti.roller.coasters.presentation.settings.model.SettingsAction.App
 import com.sottti.roller.coasters.presentation.settings.model.SettingsAction.AppThemeActions.LaunchAppThemePicker
 import com.sottti.roller.coasters.presentation.settings.model.SettingsAction.DynamicColorCheckedChange
 import com.sottti.roller.coasters.presentation.settings.model.SettingsState
+import com.sottti.roller.coasters.presentation.utils.Spacer
 import com.sottti.roller.coasters.presentation.utils.plus
 
 @Composable
@@ -60,9 +67,13 @@ internal fun SettingsList(
         modifier = Modifier.nestedScroll(nestedScrollConnection),
     ) {
         dynamicColor(state = state, onDynamicColorCheckedChange = onDynamicColorCheckedChange)
+        item { Spacer(dimensions.padding.extraSmall) }
         theme(state = state, launchThemePicker = launchThemePicker)
+        item { Spacer(dimensions.padding.mediumLarge) }
         colorContrast(state = state, launchColorContrastPicker = launchColorContrastPicker)
+        item { Spacer(dimensions.padding.extraSmall) }
         language(state = state, launchLanguagePicker = launchLanguagePicker)
+        item { Spacer(dimensions.padding.extraSmall) }
         measurementSystem(
             launchMeasurementSystemPicker = launchMeasurementSystemPicker,
             state = state,
@@ -75,11 +86,20 @@ private fun LazyListScope.dynamicColor(
     onDynamicColorCheckedChange: (Boolean) -> Unit,
 ) {
     state.dynamicColor?.let { dynamicState ->
+        val onClick = when (dynamicState.checkedState) {
+            is DynamicColorCheckedState.Loaded -> {
+                { onDynamicColorCheckedChange(!dynamicState.checkedState.checked) }
+            }
+
+            DynamicColorCheckedState.Loading -> null
+        }
         settingItem(
             headline = dynamicState.headline,
             supporting = dynamicState.supporting,
             iconState = dynamicState.icon,
-            onClick = null,
+            onClick = onClick,
+            topRounded = true,
+            bottomRounded = false,
             key = "dynamic_color"
         ) {
             when (dynamicState.checkedState) {
@@ -103,6 +123,8 @@ private fun LazyListScope.theme(
         supporting = state.appTheme.listItem.supporting,
         iconState = state.appTheme.listItem.icon,
         onClick = launchThemePicker,
+        topRounded = false,
+        bottomRounded = true,
         key = "theme",
     ) {
         when (val selection = state.appTheme.listItem.selectedAppTheme) {
@@ -121,6 +143,8 @@ private fun LazyListScope.colorContrast(
         supporting = state.appColorContrast.listItem.supporting,
         iconState = state.appColorContrast.listItem.icon,
         onClick = launchColorContrastPicker,
+        topRounded = true,
+        bottomRounded = false,
         key = "color_contrast",
     ) {
         when (val selection = state.appColorContrast.listItem.selectedAppColorContrast) {
@@ -139,6 +163,8 @@ private fun LazyListScope.language(
         supporting = state.appLanguage.listItem.supporting,
         iconState = state.appLanguage.listItem.icon,
         onClick = launchLanguagePicker,
+        topRounded = false,
+        bottomRounded = false,
         key = "language",
     ) {
         when (val selection = state.appLanguage.listItem.selectedAppLanguage) {
@@ -157,6 +183,8 @@ private fun LazyListScope.measurementSystem(
         supporting = state.appMeasurementSystem.listItem.supporting,
         iconState = state.appMeasurementSystem.listItem.icon,
         onClick = launchMeasurementSystemPicker,
+        topRounded = false,
+        bottomRounded = true,
         key = "measurement_system",
     ) {
         when (val selection =
@@ -172,17 +200,57 @@ private fun LazyListScope.settingItem(
     @StringRes supporting: Int,
     iconState: IconState,
     onClick: (() -> Unit)?,
+    topRounded: Boolean,
+    bottomRounded: Boolean,
     key: String,
     trailingContent: @Composable () -> Unit,
 ) {
     item(key = key) {
-        ListItem(
-            modifier = onClick?.let { Modifier.clickable(onClick = onClick) } ?: Modifier,
-            headlineContent = { Text.Vanilla(headline) },
-            leadingContent = { Icon(iconState) },
-            supportingContent = { Text.Vanilla(supporting) },
-            trailingContent = trailingContent,
-        )
+        SettingsListCard(
+            topRounded = topRounded,
+            bottomRounded = bottomRounded,
+            onClick = onClick,
+        ) {
+            ListItem(
+                modifier = Modifier.fillMaxWidth(),
+                colors = ListItemDefaults.colors().copy(containerColor = Color.Transparent),
+                headlineContent = { Text.Vanilla(headline) },
+                leadingContent = { CircledIcon(iconState) },
+                supportingContent = { Text.Vanilla(supporting) },
+                trailingContent = trailingContent,
+            )
+        }
+    }
+}
+
+@Composable
+private fun SettingsListCard(
+    topRounded: Boolean,
+    bottomRounded: Boolean,
+    onClick: (() -> Unit)?,
+    content: @Composable () -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = dimensions.padding.medium),
+        onClick = onClick ?: {},
+        shape = when {
+            topRounded && bottomRounded -> MaterialTheme.shapes.large
+            topRounded -> MaterialTheme.shapes.large.copy(
+                bottomStart = MaterialTheme.shapes.extraSmall.bottomStart,
+                bottomEnd = MaterialTheme.shapes.extraSmall.bottomEnd,
+            )
+
+            bottomRounded -> MaterialTheme.shapes.large.copy(
+                topStart = MaterialTheme.shapes.extraSmall.topStart,
+                topEnd = MaterialTheme.shapes.extraSmall.topEnd,
+            )
+
+            else -> MaterialTheme.shapes.extraSmall
+        },
+    ) {
+        content()
     }
 }
 
