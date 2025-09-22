d4381ef968dcb2798319125c8b4d7a62fb98ef44
Pablo Costa Tirado <sottti@gmail.com>
2025-09-22 22:57:44 +0200

    - Hides away references to MaterialTheme.* in favor of RollerCoastersTheme.kt - Merges spacing and padding tokens. - Adds shapes to the RollerCoastersTheme.kt

--- presentation/home/src/main/kotlin/com/sottti/roller/coasters/presentation/home/ui/HomeActivity.kt
+++ presentation/home/src/main/kotlin/com/sottti/roller/coasters/presentation/home/ui/HomeActivity.kt
@@ -3,16 +3,14 @@ package com.sottti.roller.coasters.presentation.home.ui
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
 import com.sottti.roller.coasters.presentation.design.system.themes.RollerCoastersTheme
 import dagger.hilt.android.AndroidEntryPoint
 
-@AndroidEntryPoint // Is this annotation needed?
-// This could be a ComponentActivity I think, but I'd like to explore how that affects
-// the runtime for config changes like light/dark
-internal class HomeActivity : AppCompatActivity() {
+@AndroidEntryPoint
+internal class HomeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
