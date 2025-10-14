7a075306690f189360ae338227af3d90b01d6c9b
Pablo Costa Tirado <sottti@gmail.com>
2025-10-14 22:20:42 +0200

    Playground stuff.

---
+++ presentation/design-system/playground/src/main/kotlin/com/sottti/roller/coasters/presentation/design/system/playground/Text.kt
@@ -0,0 +1,63 @@
+package com.sottti.roller.coasters.presentation.design.system.playground
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sottti.roller.coasters.presentation.design.system.text.Text
+import com.sottti.roller.coasters.presentation.design.system.themes.RollerCoastersPreviewTheme
+import androidx.compose.material3.Text as MaterialText
+
+
+@Preview
+@Composable
+private fun Material3Text() {
+    RollerCoastersPreviewTheme() {
+        MyCard {
+            MaterialText(text = "This is a Material 3 Text")
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun VanillaText() {
+    RollerCoastersPreviewTheme() {
+        MyCard {
+            Text.Vanilla(text = "This is a Material 3 Text")
+        }
+    }
+}
+
+
+@Preview
+@Composable
+private fun BodyMediumText() {
+    RollerCoastersPreviewTheme() {
+        MyCard {
+            Text.Body.Medium(text = "This is a Material 3 Text")
+        }
+    }
+}
+
+
+@Composable
+private fun MyCard(
+    content: @Composable () -> Unit,
+) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            contentColor = MaterialTheme.colorScheme.error,
+        )
+    ) {
+        Box(modifier = Modifier.padding(16.dp)) {
+            content()
+        }
+    }
+}
